e658d19b68a0d241337951c037a1478a4216d7d7
David Romero <65471850+LamiaceaeR@users.noreply.github.com>
2020-06-07 07:42:13 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Here's a test matrix
+M1 <- matrix(c(23,56,12,4,8,12,1,4,19), ncol = 3, nrow = 3)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#This function say if the matrix input can be solved
+makeCacheMatrix <- function(x){
+  d <- det(x)
+  if(d!=0){
+    print("its posible to inverse")
+    } else{
+      print("matrix must be square")
+    }
+  return(d)
+  return(x)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#This function solve the matrix and show the solution
+CacheSolve <- function(x){
+  MInv <- solve(x)
+  
+  return(MInv)
 }
